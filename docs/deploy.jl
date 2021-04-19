a9710f7d8d1df871ac0ae613d46be2422c35e1d6
David Muhr <muhrdavid@gmail.com>
2021-04-19 15:33:16 +0200

    try to pin versions to fix docs

--- docs/deploy.jl
+++ docs/deploy.jl
@@ -4,6 +4,7 @@ deploydocs(;
     repo="github.com/davnn/OutlierDetection.jl",
     target = "site",
     push_preview = true,
-    deps = Deps.pip("mkdocs", "mkdocs-material", "pymdown-extensions", "pygments"),
+    deps = Deps.pip("mkdocs==1.1.2", "mkdocs-material==7.1.2", "pymdown-extensions==8.1.1",
+                    "pygments==2.8.1", "Jinja2==2.11.3"),
     make = () -> run(`mkdocs build`)
 )
