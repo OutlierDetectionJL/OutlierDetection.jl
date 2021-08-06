71bba6bfa4ff1c9d5a1a16938f71a9e7a0d0c2f2
David Muhr <muhrdavid@gmail.com>
2021-08-06 11:33:15 +0200

    refer to MMI

--- src/macros.jl
+++ src/macros.jl
@@ -3,10 +3,10 @@ macro detector_model(ex)
     push!(ex.args[3].args, :(normalize::Function = $normalize))
     push!(ex.args[3].args, :(classify::Function = $classify))
     push!(ex.args[3].args, :(threshold::Real = 0.9))
-    ex, modelname, params, defaults, constraints = MLJModelInterface._process_model_def(__module__, ex)
-    const_ex = MLJModelInterface._model_constructor(modelname, params, defaults)
+    ex, modelname, params, defaults, constraints = MMI._process_model_def(__module__, ex)
+    const_ex = MMI._model_constructor(modelname, params, defaults)
     push!(ex.args[3].args, const_ex)
-    clean_ex = MLJModelInterface._model_cleaner(modelname, defaults, constraints)
+    clean_ex = MMI._model_cleaner(modelname, defaults, constraints)
     esc(
         quote
             Base.@__doc__($ex)
