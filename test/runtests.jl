eb63da679e237d371a31f72ab0027d6624a8aa06
David Muhr <muhrdavid@gmail.com>
2021-09-03 21:25:48 +0200

    reflect new interface API

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,11 +8,11 @@ const OD = OutlierDetectionInterface
 
 struct MinimalUnsupervised <: OD.UnsupervisedDetector end
 struct MinimalSupervised <: OD.SupervisedDetector end
-struct MinimalModel <: OD.Model end
+struct MinimalModel <: OD.DetectorModel end
 
 score(X) = dropdims(mean(X, dims=1), dims=1)
-OD.fit(_::MinimalUnsupervised, X::OD.Data)::Tuple{OD.Model, OD.Scores} = MinimalModel(), score(X)
-OD.fit(_::MinimalSupervised, X::OD.Data, y::OD.Labels)::Tuple{OD.Model, OD.Scores} = MinimalModel(), score(X)
+OD.fit(_::MinimalUnsupervised, X::OD.Data)::OD.Fit = MinimalModel(), score(X)
+OD.fit(_::MinimalSupervised, X::OD.Data, y::OD.Labels)::OD.Fit = MinimalModel(), score(X)
 OD.transform(_::Union{MinimalSupervised, MinimalUnsupervised}, model::MinimalModel, X::OD.Data)::OD.Scores = score(X)
 
 include("tests.jl")
