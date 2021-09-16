6fa5432779ffe6b7804c485dddc70112b68a425d
David Muhr <muhrdavid+github@gmail.com>
2021-09-16 17:05:22 +0200

    add verbosity kwarg to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,8 +11,8 @@ struct MinimalSupervised <: OD.SupervisedDetector end
 struct MinimalModel <: OD.DetectorModel end
 
 score(X) = dropdims(mean(X, dims=1), dims=1)
-OD.fit(_::MinimalUnsupervised, X::OD.Data)::OD.Fit = MinimalModel(), score(X)
-OD.fit(_::MinimalSupervised, X::OD.Data, y::OD.Labels)::OD.Fit = MinimalModel(), score(X)
-OD.transform(_::Union{MinimalSupervised, MinimalUnsupervised}, model::MinimalModel, X::OD.Data)::OD.Scores = score(X)
+OD.fit(::MinimalUnsupervised, X::OD.Data; verbosity)::OD.Fit = MinimalModel(), score(X)
+OD.fit(::MinimalSupervised, X::OD.Data, y::OD.Labels; verbosity)::OD.Fit = MinimalModel(), score(X)
+OD.transform(::Union{MinimalSupervised, MinimalUnsupervised}, model::MinimalModel, X::OD.Data)::OD.Scores = score(X)
 
 include("tests.jl")
