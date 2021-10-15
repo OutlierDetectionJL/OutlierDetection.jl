1d5e48071c31673bffbaac0fa9fd61791b305eae
David Muhr <muhrdavid@gmail.com>
2021-10-15 19:52:57 +0200

    add augmented_transform

--- src/mlj_transformers.jl
+++ src/mlj_transformers.jl
@@ -39,3 +39,53 @@ function to_scores(normalize::Function, combine::Function, scores::Tuple{Scores,
     scores_train, scores_test = combine(reduce_cat(1)), combine(reduce_cat(2))
     scores_train, scores_test
 end
+
+to_fitresult(mach::MLJ.Machine{<:OD.Detector})::Fit = (mach.fitresult, mach.report.scores)
+
+# this includes all composites defined in mlj_wrappers.jl
+const DetectorComposites = Union{
+    MLJ.Machine{<:MLJ.SupervisedDetectorComposite},
+    MLJ.Machine{<:MLJ.UnsupervisedDetectorComposite},
+    MLJ.Machine{<:MLJ.ProbabilisticUnsupervisedDetectorComposite},
+    MLJ.Machine{<:MLJ.DeterministicUnsupervisedDetectorComposite},
+    MLJ.Machine{<:MLJ.ProbabilisticSupervisedDetectorComposite},
+    MLJ.Machine{<:MLJ.DeterministicSupervisedDetectorComposite}
+}
+
+function check_mach(mach)
+    # catch deserialized machine with no data:
+    isempty(mach.args) && MLJ._err_serialized(augmented_transform)
+    # catch not-yet-trained machine:
+    mach.state > 0 || error("$mach has not been trained.")
+end
+
+# 0. augmented_transform given rows:
+function OD.augmented_transform(mach::MLJ.Machine{<:OD.Detector}; rows=:)
+    check_mach(mach)
+    return augmented_transform(mach.model, to_fitresult(mach), selectrows(mach.model, rows, mach.data[1])...)
+end
+
+function OD.augmented_transform(mach::DetectorComposites; rows=:)
+    check_mach(mach)
+    scores_train = mach.report.scores
+    scores_test = mach.fitresult.transform(selectrows(mach.model, rows, mach.data[1])...)
+    return scores_train, scores_test
+end
+
+# 1. augmented_transform on machines, given *concrete* data:
+function OD.augmented_transform(mach::MLJ.Machine{<:OD.Detector}, X)
+    check_mach(mach)
+    return augmented_transform(mach.model, to_fitresult(mach), reformat(mach.model, X)...)
+end
+
+function OD.augmented_transform(mach::DetectorComposites, X)
+    check_mach(mach)
+    scores_train = mach.report.scores
+    scores_test = mach.fitresult.transform(X)
+    return scores_train, scores_test
+end
+
+# 2. operations on machines, given *dynamic* data (nodes):
+function OD.augmented_transform(mach::MLJ.Machine{<:OD.Detector}, X::MLJ.AbstractNode)
+    MLJ.node(augmented_transform, mach, X)
+end
