1d0fe10158961fb1d3ffddc5ff7a7fc3be53e81e
David Muhr <muhrdavid@gmail.com>
2021-11-03 16:40:28 +0100

    update normalization to tuple based api

--- src/normalization.jl
+++ src/normalization.jl
@@ -2,31 +2,27 @@ using Statistics:mean, std
 using SpecialFunctions:erf
 
 """
-scale_minmax(scoresTrain,
-             scoresTest)
+    scale_minmax(scores)
 
 Transform an array of scores into a range between [0,1] using min-max scaling.
 
 Parameters
 ----------
-      scores_train::AbstractVector{<:Real}
-A vector of training scores, typically the result of [`fit`](@ref) with a detector.
-
-    scores_test::AbstractVector{<:Real}
-A vector of test scores, typically the [`transform`](@ref) result of a fitted detector.
+      scores::Tuple{Scores, Scores}
+A tuple consisting of two vectors representing training and test scores.
 
 Returns
 ----------
-normalized_scores::Tuple{AbstractVector{<:Real}, AbstractVector{<:Real}}
+    normalized_scores::Tuple{Scores, Scores}
 The normalized train and test scores.
 
 Examples
 ----------
-scores_train, scores_test = ([1, 2, 3], [4, 3, 2, 1, 0])
-scale_minmax(scores_train, scores_test) # ([0.0, 0.5, 1.0], [1.0, 1.0, 0.5, 0.0, 0.0])
-scale_minmax(scores_train) # [0.0, 0.5, 1.0]
+    scores_train, scores_test = ([1, 2, 3], [4, 3, 2, 1, 0])
+    scale_minmax(scores_train, scores_test) # ([0.0, 0.5, 1.0], [1.0, 1.0, 0.5, 0.0, 0.0])
 """
-function scale_minmax(scores_train::Scores, scores_test::Scores)::Tuple{Scores, Scores}
+function scale_minmax(scores::Tuple{Scores, Scores})::Tuple{Scores, Scores}
+    scores_train, scores_test = scores
     minTrain, maxTrain = extrema(scores_train)
     @assert minTrain < maxTrain "Cannot normalize scores if they are all equal"
     f = scores -> clamp.((scores .- minTrain) ./ (maxTrain - minTrain), 0, 1)
@@ -34,35 +30,31 @@ function scale_minmax(scores_train::Scores, scores_test::Scores)::Tuple{Scores, 
 end
 
 """
-scale_unify(scores_train,
-            scores_test)
+    scale_unify(scores)
 
 Transform an array of scores into a range between [0,1] using unifying scores as described in [1].
 
 Parameters
 ----------
-    scores_train::AbstractVector{<:Real}
-A vector of training scores, typically the result of [`fit`](@ref) with a detector.
-
-    scores_test::AbstractVector{<:Real}
-A vector of test scores, typically the [`transform`](@ref) result of a fitted detector.
+    scores::Tuple{Scores, Scores}
+A tuple consisting of two vectors representing training and test scores.
 
 Returns
 ----------
-unified_scores::Tuple{AbstractVector{<:Real}, AbstractVector{<:Real}}
+    unified_scores::Tuple{Scores, Scores}
 The unified train and test scores.
 
 Examples
 ----------
-scores_train, scores_test = ([1, 2, 3], [4, 3, 2, 1, 0])
-unify(scores_train, scores_test) # ([0.0, 0.0, 0.68..], [0.95.., 0.68.., 0.0, 0.0, 0.0])
-unify(scores_train) # [0.0, 0.0, 0.68..]
+    scores_train, scores_test = ([1, 2, 3], [4, 3, 2, 1, 0])
+    unify(scores_train, scores_test) # ([0.0, 0.0, 0.68..], [0.95.., 0.68.., 0.0, 0.0, 0.0])
 
 References
 ----------
 Kriegel, Hans-Peter; Kroger, Peer; Schubert, Erich; Zimek, Arthur (2011): Interpreting and Unifying Outlier Scores.
 """
-function scale_unify(scores_train::Scores, scores_test::Scores)::Tuple{Scores, Scores}
+function scale_unify(scores::Tuple{Scores, Scores})::Tuple{Scores, Scores}
+    scores_train, scores_test = scores
     μ, σ = mean(scores_train), std(scores_train)
     @assert σ > 0 "Cannot normalize scores if they are all equal"
     f = scores -> clamp.(erf.((scores .- μ) ./ (σ * √2)), 0, 1)
