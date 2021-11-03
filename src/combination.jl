4bd4fd90de4fbd142e506e4e53764be906700564
David Muhr <muhrdavid@gmail.com>
2021-11-03 16:38:46 +0100

    update combination api to use tuples

--- src/combination.jl
+++ src/combination.jl
@@ -1,6 +1,9 @@
 using Statistics:mean, median
 
-# TODO: Add AOM/MOA/LSCP combination strategies 
+# TODO: Add AOM/MOA/LSCP combination strategies, see https://arxiv.org/pdf/1812.01528.pdf
+
+# Helper function to turn tuples of scores into matrices
+combine(f, scores::Tuple{Scores, Scores}...) = (f(reduce(hcat, first.(scores))), f(reduce(hcat, last.(scores))))
 
 """
     combine_mean(scores_mat)
@@ -23,6 +26,7 @@ Examples
     combine_mean(scores) # [1.5, 3.5, 5.5]
 """
 combine_mean(scores_mat::AbstractMatrix{<:Real}) = dropdims(mean(scores_mat, dims=2), dims=2)
+combine_mean(scores::Tuple{Scores, Scores}...) = combine(combine_mean, scores...)
 combine_mean(scores::Scores...) = combine_mean(hcat(scores...))
 
 """
@@ -46,6 +50,7 @@ Examples
     combine_median(scores) # [1.5, 3.5, 5.5]
 """
 combine_median(scores_mat::AbstractMatrix{<:Real}) = dropdims(median(scores_mat, dims=2), dims=2)
+combine_median(scores::Tuple{Scores, Scores}...) = combine(combine_median, scores...)
 combine_median(scores::Scores...) = combine_median(hcat(scores...))
 
 """
@@ -69,4 +74,5 @@ Examples
     combine_max(scores) # [2, 4, 6]
 """
 combine_max(scores_mat::AbstractMatrix{<:Real}) = dropdims(maximum(scores_mat, dims=2), dims=2)
+combine_max(scores::Tuple{Scores, Scores}...) = combine(combine_max, scores...)
 combine_max(scores::Scores...) = combine_max(hcat(scores...))
