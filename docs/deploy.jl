fa81057bb86c0cc5f0d9b13e972b20201d7d591e
David Muhr <muhrdavid@gmail.com>
2021-11-08 17:54:11 +0100

    don't push doc previews

--- docs/deploy.jl
+++ docs/deploy.jl
@@ -3,7 +3,7 @@ using Documenter
 deploydocs(;
     repo="github.com/OutlierDetectionJL/OutlierDetection.jl",
     target = "site",
-    push_preview = true,
+    push_preview = false,
     # mkdocs-material bundles all other required dependencies
     deps = Deps.pip("mkdocs-material==7.3.6"),
     make = () -> run(`mkdocs build`)
