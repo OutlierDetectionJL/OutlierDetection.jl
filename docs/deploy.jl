f83ad08748d119c46c56314323b1f37eed920aba
David Muhr <muhrdavid@gmail.com>
2021-11-08 16:38:45 +0100

    update docs pip deps

--- docs/deploy.jl
+++ docs/deploy.jl
@@ -4,7 +4,7 @@ deploydocs(;
     repo="github.com/OutlierDetectionJL/OutlierDetection.jl",
     target = "site",
     push_preview = true,
-    deps = Deps.pip("mkdocs==1.1.2", "mkdocs-material==7.1.2", "pymdown-extensions==8.1.1",
-                    "pygments==2.8.1", "Jinja2==2.11.3"),
+    # mkdocs-material bundles all other required dependencies
+    deps = Deps.pip("mkdocs-material==7.3.6"),
     make = () -> run(`mkdocs build`)
 )
